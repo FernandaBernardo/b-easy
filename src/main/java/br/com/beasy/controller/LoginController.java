c2786482a1aa12ef3dca1c179ef5f42c78cadb44
Fernanda Bernardo <fernandabernardo94@gmail.com>
2015-10-25 22:05:55 -0200

    refactoring facebook api

--- src/main/java/br/com/beasy/controller/LoginController.java
+++ src/main/java/br/com/beasy/controller/LoginController.java
@@ -2,23 +2,12 @@ package br.com.beasy.controller;
 
 import javax.inject.Inject;
 
-import org.scribe.builder.ServiceBuilder;
-import org.scribe.builder.api.FacebookApi;
-import org.scribe.model.OAuthRequest;
 import org.scribe.model.Response;
-import org.scribe.model.Token;
-import org.scribe.model.Verb;
-import org.scribe.model.Verifier;
-import org.scribe.oauth.OAuthService;
-
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import br.com.beasy.dao.UserDao;
 import br.com.beasy.model.LoggedUser;
 import br.com.beasy.model.User;
-import br.com.beasy.model.UserType;
+import br.com.beasy.social.FacebookAPI;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
@@ -63,34 +52,20 @@ public class LoginController {
 	
 	@Get("/facebook")
 	public void requestToken() {
-		String url = getOAuthService().getAuthorizationUrl(null);
+		FacebookAPI facebookApi = new FacebookAPI(env);
+		String url = facebookApi.getOAuthService().getAuthorizationUrl(null);
 		result.redirectTo(url);
 	}
 	
 	@Get("/facebook/login")
 	public void login(String code) {
-		OAuthService service = getOAuthService();
-		Verifier verifier = new Verifier(code);
-		Token token = service.getAccessToken(null, verifier);
+		FacebookAPI facebookApi = new FacebookAPI(env);
+		facebookApi.getOAuthService();
+		facebookApi.getAccessToken(code);
 		
-		OAuthRequest request = new OAuthRequest(Verb.GET, "https://graph.facebook.com/me?fields=email,name,id");
-	    service.signRequest(token, request);
-	    Response response = request.send();
+		Response response = facebookApi.makeRequest("https://graph.facebook.com/me?fields=email,name,id");
 		
-	    String responseBody = response.getBody();
-	    
-	    JsonObject jsonObject = new JsonParser().parse(responseBody).getAsJsonObject();
-	    
-	    JsonElement id = jsonObject.get("id");
-	    JsonElement name = jsonObject.get("name");
-	    JsonElement email = jsonObject.get("email");
-	    
-	    User user = new User();
-		user.setName(name != null ? name.getAsString() : null);
-		user.setEmail(email != null ? email.getAsString() : null);
-		user.setFacebookId(id != null ? id.getAsString() : null);
-		user.setUserType(UserType.FACEBOOK);
-	    
+	    User user = facebookApi.getUserFromFacebookJson(response.getBody());
 	    if(!dao.userExist(user)){
 	    	dao.addUser(user);
 		} 
@@ -103,15 +78,5 @@ public class LoginController {
 	public void logout() {
 		loggedUser.logout();
 		result.redirectTo(HomeController.class).home();
-	}
-	
-	private OAuthService getOAuthService() {
-		return new ServiceBuilder()
-		.provider(FacebookApi.class)
-		.apiKey(env.get("facebook.api.key"))
-		.apiSecret(env.get("facebook.api.secret"))
-		.callback("http://localhost:8080/b-easy/facebook/login")
-		.scope("email")
-		.build();
 	}
 }
---
+++ src/main/java/br/com/beasy/social/FacebookAPI.java
@@ -0,0 +1,69 @@
+package br.com.beasy.social;
+
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.builder.api.FacebookApi;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.model.Verifier;
+import org.scribe.oauth.OAuthService;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import br.com.beasy.model.User;
+import br.com.beasy.model.UserType;
+import br.com.caelum.vraptor.environment.Environment;
+
+public class FacebookAPI {
+	private Token accessToken;
+	private Environment env;
+	private OAuthService service;
+
+	public FacebookAPI(Environment env) {
+		this.env = env;
+	}
+	
+	public OAuthService getOAuthService() {
+		this.service = new ServiceBuilder()
+		.provider(FacebookApi.class)
+		.apiKey(env.get("facebook.api.key"))
+		.apiSecret(env.get("facebook.api.secret"))
+		.callback("http://localhost:8080/b-easy/facebook/login")
+		.scope("email")
+		.build();
+		
+		return service;
+	}
+	
+	public Token getAccessToken(String code) {
+		Verifier verifier = new Verifier(code);
+		this.accessToken = service.getAccessToken(null, verifier);
+		return accessToken;
+	}
+	
+	public Response makeRequest(String url) {
+		OAuthRequest request = new OAuthRequest(Verb.GET, url);
+	    service.signRequest(accessToken, request);
+	    Response response = request.send();
+	    return response;
+	}
+
+	public User getUserFromFacebookJson(String responseBody) {
+		JsonObject jsonObject = new JsonParser().parse(responseBody).getAsJsonObject();
+	    
+	    JsonElement id = jsonObject.get("id");
+	    JsonElement name = jsonObject.get("name");
+	    JsonElement email = jsonObject.get("email");
+	    
+	    User user = new User();
+		user.setName(name != null ? name.getAsString() : null);
+		user.setEmail(email != null ? email.getAsString() : null);
+		user.setFacebookId(id != null ? id.getAsString() : null);
+		user.setUserType(UserType.FACEBOOK);
+		
+		return user;
+	}
+}
