7d44fb4c90d4825ff5ea25b9d5061ebe5ad0c8a9
Fernanda Bernardo <fernandabernardo94@gmail.com>
2015-11-24 19:25:24 -0200

    fix change status

--- src/main/java/br/com/beasy/controller/DashboardController.java
+++ src/main/java/br/com/beasy/controller/DashboardController.java
@@ -62,7 +62,7 @@ public class DashboardController {
 	@Get("/{subject.id}/tarefas/{task.id}/{status}")
 	@AuthenticationRequired
 	public void changeTask(Subject subject, Task task, Status toStatus) {
-		
-		result.forwardTo(DashboardController.class).showTasks(subject);;
+		task.setStatus(toStatus);
+		taskDao.updateTask(task);
 	}
 }
